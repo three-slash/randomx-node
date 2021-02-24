6de02c1e4ab32c152584709e6f5db00a8d898752
Ribhararnus Pracutian <oknoorap@gmail.com>
2021-02-24 19:23:38 +0700

    fix: update

--- randomx.js
+++ randomx.js
@@ -1,6 +1,29 @@
 var rx = require("./build/Release/randomx.node");
 
-function generate(seed, hashes) {
+function batchVM(vm, hashes) {
+  if (!vm) {
+    throw new Error("Invalid VM");
+  }
+
+  if (!Array.isArray(hashes)) {
+    throw new Error("Invalid hashes, hashes should be an Array of string");
+  }
+
+  return new Promise((resolve, reject) => {
+    const result = [];
+
+    try {
+      for (const hash of hashes) {
+        result.push(rx.hash(vm, hash));
+      }
+      resolve(result);
+    } catch (err) {
+      reject(err);
+    }
+  });
+}
+
+function batch(seed, hashes) {
   if (typeof seed !== "string") {
     throw new Error("Invalid seed, seed should be a String");
   }
@@ -25,4 +48,23 @@ function generate(seed, hashes) {
   });
 }
 
-module.exports = generate;
+function createVM(seed) {
+  if (typeof seed !== "string") {
+    throw new Error("Invalid seed, seed should be a String");
+  }
+
+  return new Promise((resolve, reject) => {
+    let vm;
+
+    try {
+      vm = rx.RandomxVM(seed, ["jit", "ssse3"]);
+      resolve(vm);
+    } catch (err) {
+      reject(err);
+    }
+  });
+}
+
+exports.batch = batch;
+exports.createVM = createVM;
+exports.batchVM = batchVM;
