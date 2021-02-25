640184c5ca214b0e6d0659814b14048e714d5c83
Ribhararnus Pracutian <oknoorap@gmail.com>
2021-02-25 09:23:45 +0700

    fix: init script

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-if [ -d .git ]; then
+if [ ! -d .git ]; then
   git init
+  git remote add origin https://github.com/three-dash/randomx-node
+  git submodule add https://github.com/tevador/RandomX RandomX
 fi;
 git submodule update --init --recursive
